44a7aca5e2075b0c7547978abd850c327bf4a68c
Cris3030 <118198103+Cris3030@users.noreply.github.com>
2022-12-04 22:54:11 -0400

    Add files via upload

---
+++ search.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Search results</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" href="images/favicon.ico" type="image/x-icon">
+    <link rel="shortcut icon" href="images/favicon.ico" type="image/x-icon" />
+    <meta name="description" content="Your description">
+    <meta name="keywords" content="Your keywords">
+    <meta name="author" content="Your name">    
+    <link rel="stylesheet" href="css/bootstrap.css" type="text/css" media="screen">
+    <link rel="stylesheet" href="css/style.css" type="text/css" media="screen">
+    <script type="text/javascript" src="js/jquery.js"></script>
+    <script type="text/javascript" src="search/search.js"></script>
+    <script src="js/superfish.js"></script>
+    <script src="js/jquery.mobilemenu.js"></script>
+    <script src="js/jquery.animate-colors-min.js"></script>
+    <script src="js/jquery.easing.1.3.js"></script>
+    <script src="js/jquery.ui.totop.js"></script>
+    <script src="js/jquery.touchSwipe.min.js"></script>
+    <script src="js/jquery.equalheights.js"></script>
+    
+  <!--[if lt IE 9]>
+    <link rel="stylesheet" href="assets/tm/css/tm_docs.css" type="text/css" media="screen">
+    <script src="assets/assets/js/html5shiv.js"></script>    
+    <script src="assets/assets/js/respond.min.js"></script>
+    <div style='text-align:center'><a href="http://windows.microsoft.com/en-US/internet-explorer/products/ie/home?ocid=ie6_countdown_bannercode"><img src="http://storage.ie6countdown.com/assets/100/images/banners/warning_bar_0000_us.jpg" border="0" height="42" width="820" alt="You are using an outdated browser. For a faster, safer browsing experience, upgrade for free today." /></a></div>  
+  <![endif]-->
+</head>
+<body class="bg2">
+<!--==============================header=================================-->
+<header id="header" class="bgheader">
+    <div class="width_1">
+          <h1 class="navbar-brand navbar-brand_"><a href="index.html"><img alt="Monique" src="img/logo.png"></a></h1>
+          <div class="menuheader">
+            <nav class="navbar navbar-default navbar-static-top tm_navbar" role="navigation">
+                <ul class="nav sf-menu">
+                  <li><a href="index.html">main page</a></li>
+                  <li><a href="index-1.html">about us</a>
+                    <ul>
+                      <li><img src="img/arrowup.png" alt=""><a href="#">history</a></li>
+                      <li><a href="#">staff</a></li>
+                      <li><a class="last" href="#">news</a>
+                        <ul>
+                           <li><a href="#">fresh</a></li>
+                           <li><a class="last" href="#">archive</a></li>                       
+                        </ul>
+                      </li>
+                    </ul>
+                  </li>
+                  <li><a href="index-2.html">rooms</a></li>
+                  <li><a href="index-3.html">latest news</a></li>
+                  <li><a href="index-4.html">contacts</a></li>
+                </ul>
+            </nav>
+          </div>
+    </div>
+</header>
+<!--==============================content=================================-->
+<section id="content">
+<div class="row_14">
+  <div class="container">
+    <div class="row">
+      <div class="col-lg-12 col-md-12 col-sm-12">
+       <h2 class="h2-pad">Search result:</h2>
+        <div id="search-results"></div>
+      </div>
+    </div>
+  </div>
+</div>
+</section>
+<!--==============================footer=================================-->
+<footer>
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-5 col-md-5 col-sm-7 pull-right">
+                <a class="maillink1" href="#">info@demolink.org</a>
+                <div class="line6"></div>
+                <ul class="social_icons clearfix">
+                     <li><a href="#"><img src="img/follow_icon1.png" alt=""></a></li>
+                     <li><a href="#"><img src="img/follow_icon2.png" alt=""></a></li>
+                     <li><a href="#"><img src="img/follow_icon3.png" alt=""></a></li>
+                     <li><a href="#"><img src="img/follow_icon4.png" alt=""></a></li>
+                </ul>
+            </div>
+            <div class="col-lg-7 col-md-5 col-sm-5 pull-left">
+                <p class="footerpriv">Monique &copy; 2013 <img src="img/bulletfooter.jpg" alt=""> <a class="privacylink" href="index-5.html">Privacy policy</a></p>
+            </div>
+        </div>
+    </div>
+</footer>
+<script src="js/bootstrap.min.js"></script>
+<script src="js/tm-scripts.js"></script>
+</body>
+</html>
